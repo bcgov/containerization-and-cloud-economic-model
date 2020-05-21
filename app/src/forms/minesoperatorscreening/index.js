393212c65697ac1cd8a38db9acdccd5c90c7f512
Jason Sherman <jsherman@parcsystems.ca>
2020-05-21 09:01:09 -0700

    commit just to kick off the build, last one used a really old image...?

--- app/src/forms/minesoperatorscreening/index.js
+++ app/src/forms/minesoperatorscreening/index.js
@@ -1,7 +1,7 @@
 const constants = require('./constants');
 const middleware = require('./middleware');
 const routes = require('./routes');
-
+// slug will be the paths.
 module.exports.mount = (app) => {
   const p = `/${constants.SLUG}`;
   app.use(p, routes);
