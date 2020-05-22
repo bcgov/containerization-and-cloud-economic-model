89a6649f306baacf902310b6ddb1d954a927fd4d
Lucas O'Neil <lucasoneil@gmail.com>
2020-05-21 22:47:33 -0700

    Initial settings page

---
+++ app/frontend/src/components/minesoperatorscreening/admin/settings/SettingItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>
+      <strong>Last Updated:</strong>
+      {{ updatedAtDisplay }}
+    </p>
+    <pre>{{ JSON.stringify(item.config, null, 2) }}</pre>
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+
+//import minesOperatorScreeningService from '@/services/minesOperatorScreeningService';
+
+export default {
+  name: 'SettingItem',
+  props: {
+    item: {
+      required: true,
+      type: Object,
+    }
+  },
+  computed: {
+    updatedAtDisplay() {
+      return `${moment(this.item.updatedAt).format('MMMM D YYYY, h:mm:ss a')}${this.item.updatedBy ? ' - ' + this.item.updatedBy : ''}`;
+    }
+  }
+};
+</script>
---
+++ app/frontend/src/components/minesoperatorscreening/admin/settings/SettingsPanel.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <v-progress-linear indeterminate v-if="gettingSettings" color="primary" class="mb-2" />
+
+    <v-alert v-if="error" type="error" tile dense>{{ error }}</v-alert>
+
+    <div v-if="!gettingSettings">
+      <v-expansion-panels>
+        <v-expansion-panel v-for="item in treeItems" :key="item.name">
+          <v-expansion-panel-header>{{ item.name }}</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <SettingItem :item="item" />
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import minesOperatorScreeningService from '@/services/minesOperatorScreeningService';
+import SettingItem from '@/components/minesoperatorscreening/admin/settings/SettingItem.vue';
+
+export default {
+  name: 'SettingsPanel',
+  components: {
+    SettingItem,
+  },
+  data: () => ({
+    error: false,
+    gettingSettings: false,
+    settings: {},
+    treeItems: [],
+  }),
+  methods: {
+    async getSettings() {
+      this.error = '';
+      this.gettingSettings = true;
+      try {
+        const response = await minesOperatorScreeningService.getSettings();
+        this.settings = response.data;
+        this.treeItems = response.data
+          .filter(s => s.enabled);
+      } catch (error) {
+        console.log(`Error occurred getting Settings: ${error}`); // eslint-disable-line no-console
+        this.error = 'Failed to fetch Settings from the Database';
+      } finally {
+        this.gettingSettings = false;
+      }
+    }
+  },
+  created() {
+    this.getSettings();
+  }
+};
+</script>
+
+<style  scoped>
+.v-expansion-panel-header {
+  font-weight: bold;
+  color: #003366 !important;
+  font-size: 1.1em;
+}
+</style>
