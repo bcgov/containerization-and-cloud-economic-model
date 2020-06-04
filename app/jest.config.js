ca770b5e3b1102908c88aafdf73a9b58d9f25918
Jeremy Ho <jujaga@gmail.com>
2020-06-04 10:11:58 -0700

    Supress lint warning with console log Drop coverage gathering from db/migrations folder
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   clearMocks: true,
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.js', '!frontend/**/*.*'],
+  collectCoverageFrom: ['src/**/*.js', '!frontend/**/*.*', '!src/db/migrations/*.*'],
   moduleFileExtensions: ['js', 'json'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
