380d2d1549b2f92d6d4f0018e0f57e703be71f14
Lucas O'Neil <lucasoneil@gmail.com>
2020-06-12 11:49:35 -0700

    Add seafood pdf

--- app/frontend/src/components/agriseafoodopscreening/Landing.vue
+++ app/frontend/src/components/agriseafoodopscreening/Landing.vue
@@ -55,7 +55,23 @@
                   <a
                     href="https://www2.gov.bc.ca/assets/gov/health/about-bc-s-health-care-system/office-of-the-provincial-health-officer/covid-19/covid-19-pho-guidance-farms-farm-workers.pdf"
                     target="_blank"
-                    data-test="btn-form-health-pdf-link"
+                    data-test="btn-form-health-pdf-link-1"
+                  >
+                    Download PDF
+                    <v-icon small color="primary">open_in_new</v-icon>
+                  </a>
+                </p>
+                <h4 class="mb-4">
+                  <strong>
+                    Read
+                    <em>COVID-19 pandemic PHO Order Industrial Camps: What does it mean for B.C.'s agriculture and seafood industry?</em>
+                  </strong>
+                </h4>
+                <p>
+                  <a
+                    href="https://www2.gov.bc.ca/assets/gov/farming-natural-resources-and-industry/agriculture-and-seafood/covid-19/agri_qa_pho_order_industrial_camps.pdf"
+                    target="_blank"
+                    data-test="btn-form-health-pdf-link-2"
                   >
                     Download PDF
                     <v-icon small color="primary">open_in_new</v-icon>
