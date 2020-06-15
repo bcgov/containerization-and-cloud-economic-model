56ee60054e15cd4df53d9600a1a3650cea208f6b
Jeremy Ho <jujaga@gmail.com>
2020-06-15 16:58:39 -0700

    Add FormList and CityLookup unit tests Refactor CityLookup to use axios instead of fetch
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

---
+++ app/frontend/tests/unit/components/FormList.spec.js
@@ -0,0 +1,37 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import Vuetify from 'vuetify';
+import Vuex from 'vuex';
+
+import FormList from '@/components/FormList.vue';
+import getRouter from '@/router';
+
+const localVue = createLocalVue();
+const router = getRouter();
+localVue.use(router);
+localVue.use(Vuetify);
+localVue.use(Vuex);
+
+describe('FormList.vue', () => {
+  let store;
+
+  beforeEach(() => {
+    store = new Vuex.Store();
+  });
+
+  it('renders', () => {
+    store.registerModule('auth', {
+      namespaced: true,
+      getters: {
+        hasResourceRoles: () => () => true,
+        keycloakReady: () => true
+      }
+    });
+
+    const wrapper = shallowMount(FormList, { localVue, store, stubs: ['BaseActionCard'] });
+
+    expect(wrapper.text()).toMatch('Agriculture and Seafood Operator Screening');
+    // expect(wrapper.text()).toMatch('Forestry Sector Operator Screening');
+    expect(wrapper.text()).toMatch('Mines Operator Screening');
+    expect(wrapper.vm.forms).toHaveLength(3);
+  });
+});
