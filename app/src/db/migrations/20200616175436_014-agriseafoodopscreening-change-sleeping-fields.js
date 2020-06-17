9f4a29c6a3b1a9949c0e440265a99b29aef467f9
Lucas O'Neil <lucasoneil@gmail.com>
2020-06-17 10:57:56 -0700

    Accidentally reverted to mispelling in migration

--- app/src/db/migrations/20200616175436_014-agriseafoodopscreening-change-sleeping-fields.js
+++ app/src/db/migrations/20200616175436_014-agriseafoodopscreening-change-sleeping-fields.js
@@ -5,7 +5,7 @@ exports.up = function (knex) {
   return Promise.resolve()
     .then(() => knex.schema.alterTable(`${AGRI_PREFIX}_submission_attestation`, table => {
       table.boolean('sharedSleepingCommunication').notNullable().defaultTo(false);
-      table.boolean('sharedSleepingProvidedAccomodations').notNullable().defaultTo(false);
+      table.boolean('sharedSleepingProvidedAccommodations').notNullable().defaultTo(false);
 
       table.dropColumn('sharedSleepingPerRoom');
       table.dropColumn('sleepingAreaType');
@@ -13,14 +13,14 @@ exports.up = function (knex) {
     }))
     .then(() => knex.schema.alterTable(`${AGRI_PREFIX}_submission_attestation`, table => {
       table.boolean('sharedSleepingCommunication').notNullable().alter();
-      table.boolean('sharedSleepingProvidedAccomodations').notNullable().alter();
+      table.boolean('sharedSleepingProvidedAccommodations').notNullable().alter();
     }));
 };
 
 exports.down = function (knex) {
   return Promise.resolve()
     .then(() => knex.schema.alterTable(`${AGRI_PREFIX}_submission_attestation`, table => {
-      table.dropColumn('sharedSleepingProvidedAccomodations');
+      table.dropColumn('sharedSleepingProvidedAccommodations');
       table.dropColumn('sharedSleepingCommunication');
 
       table.boolean('commonAreaDistancing').notNullable();
