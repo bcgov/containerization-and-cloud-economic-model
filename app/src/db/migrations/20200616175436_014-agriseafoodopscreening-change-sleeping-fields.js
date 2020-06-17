0fe0038de8b2e2b6a3c988d06e9b8446a0c6a9d6
Lucas O'Neil <lucasoneil@gmail.com>
2020-06-16 20:52:49 -0700

    Migration

---
+++ app/src/db/migrations/20200616175436_014-agriseafoodopscreening-change-sleeping-fields.js
@@ -0,0 +1,26 @@
+
+const AGRI_PREFIX = require('../../forms/attestations/agriseafoodopscreening/constants').PREFIX;
+
+exports.up = function (knex) {
+  return Promise.resolve()
+    .then(() => knex.schema.alterTable(`${AGRI_PREFIX}_submission_attestation`, table => {
+      table.boolean('sharedSleepingCommunication').notNullable().defaultTo(false);
+
+      table.dropColumn('sharedSleepingPerRoom');
+      table.dropColumn('sleepingAreaType');
+      table.dropColumn('commonAreaDistancing');
+    }))
+    .then(() => knex.schema.alterTable(`${AGRI_PREFIX}_submission_attestation`, table => {
+      table.boolean('sharedSleepingCommunication').notNullable().alter();
+    }));
+};
+
+exports.down = function (knex) {
+  return Promise.resolve()
+    .then(() => knex.schema.alterTable(`${AGRI_PREFIX}_submission_attestation`, table => {
+      table.dropColumn('sharedSleepingCommunication');
+      table.boolean('commonAreaDistancing').notNullable();
+      table.enu('sleepingAreaType', ['SINGLE', 'SHARED']).notNullable();
+      table.integer('sharedSleepingPerRoom').notNullable();
+    }));
+};
