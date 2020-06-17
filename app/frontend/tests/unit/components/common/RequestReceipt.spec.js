1ae58ea424513ba42179d5bbd9467647dd21568c
Jeremy Ho <jujaga@gmail.com>
2020-06-17 13:25:42 -0700

    Update AdminNavBar, RequestRecipt and TeamTable coverage
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/frontend/tests/unit/components/common/RequestReceipt.spec.js
+++ app/frontend/tests/unit/components/common/RequestReceipt.spec.js
@@ -17,7 +17,7 @@ describe('RequestReceipt.vue', () => {
     requestReceiptEmailSpy.mockReset();
   });
 
-  it('renders', () => {
+  it('renders when clicked', () => {
     const wrapper = shallowMount(RequestReceipt, {
       localVue,
       propsData: {
@@ -26,7 +26,50 @@ describe('RequestReceipt.vue', () => {
         submissionId: zeroUuid
       }
     });
+    wrapper.vm.displayDialog();
 
     expect(wrapper.text()).toContain('Email Receipt');
+    expect(wrapper.vm.showDialog).toBeTruthy();
+  });
+
+  it('shows success result dialog', async () => {
+    const to = 'to@foo.bar';
+    requestReceiptEmailSpy.mockResolvedValue();
+    const wrapper = shallowMount(RequestReceipt, {
+      localVue,
+      propsData: {
+        email: email,
+        formName: formName,
+        submissionId: zeroUuid
+      }
+    });
+    wrapper.setData({ to: to, valid: true });
+    await wrapper.vm.requestReceipt();
+    await wrapper.vm.$nextTick;
+
+    expect(wrapper.vm.showDialog).toBeFalsy();
+    expect(wrapper.vm.success).toBeTruthy();
+    expect(wrapper.vm.resultDialog).toBeTruthy();
+    expect(wrapper.vm.resultDialogMsg).toMatch(`An email has been sent to ${to}.`);
+  });
+
+  it('shows error result dialog', async () => {
+    requestReceiptEmailSpy.mockRejectedValue();
+    const wrapper = shallowMount(RequestReceipt, {
+      localVue,
+      propsData: {
+        email: email,
+        formName: formName,
+        submissionId: zeroUuid
+      }
+    });
+    wrapper.setData({ valid: true });
+    await wrapper.vm.requestReceipt();
+    await wrapper.vm.$nextTick;
+
+    expect(wrapper.vm.showDialog).toBeFalsy();
+    expect(wrapper.vm.success).toBeFalsy();
+    expect(wrapper.vm.resultDialog).toBeTruthy();
+    expect(wrapper.vm.resultDialogMsg).toMatch('An error occured while attempting to send your email.');
   });
 });
