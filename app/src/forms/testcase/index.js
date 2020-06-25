7b8b97d4fe816a5b11386e3feb6a67dc6deb98e6
Jason Sherman <jsherman@parcsystems.ca>
2020-06-25 08:51:54 -0700

    Add links to team management readme on main readme. Remove testcase from forms as we have new test users and have proven the team management.

--- app/src/forms/testcase/index.js
+++
@@ -1,8 +0,0 @@
-const Router = require('../teammanagement/router');
-const teamRouter = new Router('testcase');
-
-module.exports.mount = (app) => {
-  const p = '/testcase';
-  app.use(`${p}/team`, teamRouter.routes);
-  return p;
-};
