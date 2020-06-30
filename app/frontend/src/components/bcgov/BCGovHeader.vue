63d09b7a7a4ede93905ad320dd739b62e286a105
Jeremy Ho <jujaga@gmail.com>
2020-06-30 15:25:17 -0700

    Hide BCGov image on xs viewport sizes Make title font size smaller on sm and smaller viewports Ensure BCSans font is being used on title
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/frontend/src/components/bcgov/BCGovHeader.vue
+++ app/frontend/src/components/bcgov/BCGovHeader.vue
@@ -5,7 +5,7 @@
       <a href="https://www2.gov.bc.ca" data-test="btn-header-logo">
         <v-img
           alt="B.C. Government Logo"
-          class="d-flex d-md-none"
+          class="d-none d-sm-flex d-md-none"
           contain
           height="3.5rem"
           src="@/assets/images/bc_logo_square.svg"
@@ -20,7 +20,7 @@
           width="10rem"
         />
       </a>
-      <div data-test="btn-header-title" class="title">{{ appTitle }}</div>
+      <h1 data-test="btn-header-title" class="font-weight-bold title">{{ appTitle }}</h1>
       <v-spacer />
       <BaseAuthButton />
     </v-toolbar>
@@ -41,12 +41,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '@/assets/scss/style.scss';
+
 .gov-header {
   border-bottom: 2px solid #fcba19;
   .title {
+    font-family: inherit !important;
     color: #ffffff;
     overflow: hidden;
     padding: 1rem;
+    @media #{map-get($display-breakpoints, 'sm-and-down')} {
+      font-size: 1rem !important;
+    }
   }
 }
 </style>
