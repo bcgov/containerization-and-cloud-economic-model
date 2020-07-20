e9dea6e14d7b467da29a385f6a18b276f12f765e
Roland Stens <roland.stens@gmail.com>
2020-07-20 14:44:15 -0700

    Updates based on jujaga's comments

--- tests/openshift/pipelines/E2E-Test-JenkinsFile.groovy
+++ tests/openshift/pipelines/E2E-Test-JenkinsFile.groovy
@@ -1,3 +1,5 @@
+#!/usr/bin/env groovy
+
 /* Copyright 2020 Province of British Columbia
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +14,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#!/usr/bin/env groovy
 podTemplate(label: 'bddstack', name: 'bddstack', serviceAccount: 'jenkins', cloud: 'openshift', containers: [
   containerTemplate(
     name: 'jnlp',
