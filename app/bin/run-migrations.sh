c4a91ee424d6e9386567561924f591909b65ee03
Derek Roberts <derek.roberts@gmail.com>
2020-08-17 09:26:18 -0700

    Add retries/waits to node_migrate's run-migrations.sh

--- app/bin/run-migrations.sh
+++ app/bin/run-migrations.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-exec npm run migrate
+#
+set -euo nounset
+
+# Run migration with retries
+#
+while !(sleep 5 && npm run migrate); do
+  echo "Waiting 10 seconds for Postgres to start"
+  sleep 5
+done
+
+echo "Migrations complete!  Exiting."
