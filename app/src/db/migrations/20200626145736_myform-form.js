8f54909c83fc13660ee566e14c72ed124c10439d
Derek Roberts <derek.roberts@gmail.com>
2020-08-26 13:20:13 -0700

    Guide setup for sample myform

---
+++ app/src/db/migrations/20200626145736_myform-form.js
@@ -0,0 +1,21 @@
+const stamps = require('../stamps');
+
+const PREFIX = require('../../forms/myform/constants').PREFIX;
+
+exports.up = function(knex) {
+  return Promise.resolve()
+    .then(() => knex.schema.createTable(`${PREFIX}_submission_survey`, table => {
+      table.increments('surveyId').primary();
+      table.uuid('submissionId').references('submissionId').inTable(`${PREFIX}_submission`).notNullable().index();
+      table.string('submitter').notNullable();
+      table.string('answer1').notNullable();
+      table.string('answer2').notNullable();
+      table.string('answer3').nullable();
+      stamps(knex, table);
+    }));
+};
+
+exports.down = function(knex) {
+  return Promise.resolve()
+    .then(() => knex.schema.dropTableIfExists(`${PREFIX}_submission_survey`));
+};
--- app/src/routes/v1.js
+++ app/src/routes/v1.js
@@ -8,6 +8,7 @@ const agriSeafoodOpScreening = require('../forms/attestations/agriseafoodopscree
 const forestrySectorOperatorScreening = require('../forms/attestations/forestrysectoroperatorscreening');
 const form = require('../forms/form');
 const minesOperatorScreening = require('../forms/attestations/minesoperatorscreening');
+const myform = require('../forms/myform');
 
 const getSpec = () => {
   const rawSpec = fs.readFileSync(path.join(__dirname, '../docs/v1.api-spec.yaml'), 'utf8');
@@ -21,6 +22,7 @@ const agriSeafoodOpScreeningPath = agriSeafoodOpScreening.mount(router);
 const forestrySectorOperatorScreeningPath = forestrySectorOperatorScreening.mount(router);
 const formPath = form.mount(router);
 const minesOperatorScreeningPath = minesOperatorScreening.mount(router);
+const myformPath = myform.mount(router);
 
 // Base v1 Responder
 router.get('/', (_req, res) => {
@@ -30,10 +32,21 @@ router.get('/', (_req, res) => {
       '/docs',
       forestrySectorOperatorScreeningPath,
       formPath,
-      minesOperatorScreeningPath
+      minesOperatorScreeningPath,
+      myform
     ]
   });
 });
+
+// add the path to the endpoints list (optional)
+ router.get('/', (_req, res) => {
+   res.status(200).json({
+     endpoints: [
+       '/docs',
+       myformPath
+     ]
+   });
+ });
 
 /** OpenAPI Docs */
 router.get('/docs', (_req, res) => {
