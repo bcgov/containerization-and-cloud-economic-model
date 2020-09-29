e506770b0fff9fd57c00229a4e8c0897904072a9
Derek Roberts <derek.roberts@gmail.com>
2020-09-29 13:45:53 -0700

    Remove non-CEM forms from forms

--- app/src/forms/attestations/forestrysectoroperatorscreening/constants.js
+++
@@ -1,8 +0,0 @@
-const CONSTANTS = require('../constants');
-
-module.exports = Object.freeze({
-  SLUG: 'forestrysectoropscreening',
-  PREFIX: 'forsec_os',
-  TITLE: 'Forestry Sector',
-  ...CONSTANTS
-});
