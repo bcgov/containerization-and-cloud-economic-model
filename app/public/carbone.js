a8144fa33d584eb1fb7813098971df000eecf487
Derek Roberts <derek.roberts@gmail.com>
2020-10-22 17:44:23 -0700

    Comments

--- app/public/carbone.js
+++ app/public/carbone.js
@@ -1,7 +1,8 @@
+// Requires
 const fs = require('fs');
 const carbone = require('carbone');
 
-// Contexts, templates and results
+// Contexts, templates and output files
 var contextsFile = './public/CEM_contexts.json';
 var templateFile = './public/CEM_template.xlsx';
 var outputFile = './result.xlsx'
@@ -9,7 +10,7 @@ var outputFile = './result.xlsx'
 // Populate template with contexts and output it
 carboneRun(templateFile, contextsFile, outputFile);
 
-// Populate template with contexts and output it
+// Read and parse contexts file, pass to processFiles()
 function carboneRun(tf, cf, of) {
   fs.readFile(cf, 'utf8', function (err, data) {
     if (err) { throw err; }
@@ -17,7 +18,7 @@ function carboneRun(tf, cf, of) {
   });
 }
 
-// Generate a report using the sample template provided by carbone module
+// Run carbone and save output
 function processFiles(tf, contexts, of) {
   carbone.render(tf, contexts, function (err, result) {
     if (err) { return console.log(err); }
