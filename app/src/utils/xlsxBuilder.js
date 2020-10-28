89018dcfe269144f03b7e099359024213c8ccfbd
Derek Roberts <derek.roberts@gmail.com>
2020-10-28 12:10:27 -0700

    Added getContexts to xlsxBuilder.js

--- app/src/utils/xlsxBuilder.js
+++ app/src/utils/xlsxBuilder.js
@@ -37,5 +37,6 @@ function getContexts(cf) {
 }
 
 // Exports
-module.exports.carboneRun = carboneRun;
-module.exports.carboneRunPaths = carboneRunPaths;
+exports.getContexts = getContexts;
+exports.carboneRun = carboneRun;
+exports.carboneRunPaths = carboneRunPaths;
