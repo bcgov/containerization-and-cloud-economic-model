93311f20ec2be70db832061fb868d849cd9cdf34
Derek Roberts <derek.roberts@gmail.com>
2020-11-04 16:50:07 -0800

    Go const crazy

--- app/src/assets/templates/test_xlsxBuilder.js
+++ app/src/assets/templates/test_xlsxBuilder.js
@@ -3,20 +3,20 @@ const axios = require('axios').default
 const qs = require('qs')
 
 // Envars
-let CLIENT_ID = process.env.CMNSRV_CLIENTID
-let CLIENT_SECRET = process.env.CMNSRV_CLIENTSECRET
-let TOKEN_URL = process.env.COMMON_DOCGEN_SSO_ENDPOINT
+const CLIENT_ID = process.env.CMNSRV_CLIENTID
+const CLIENT_SECRET = process.env.CMNSRV_CLIENTSECRET
+const TOKEN_URL = process.env.COMMON_DOCGEN_SSO_ENDPOINT
 
 // Get token
 function get_docgen_token() {
-    let params = qs.stringify({
+    const params = qs.stringify({
         "grant_type": "client_credentials",
         "client_id": CLIENT_ID,
         "client_secret": CLIENT_SECRET,
         "scope": ""
     })
 
-    let header = {
+    const header = {
         headers: {
             "Content-Type": "application/x-www-form-urlencoded"
         }
@@ -39,7 +39,7 @@ function get_docgen_token() {
 // Returns the response content object that can be added to a starlette.responses.Response.
 async function docgen_export_to_xlsx(data, template_path, report_name) {
     // Get auth token and prepare it as an Authorization: Bearer <token> header.
-    let token = await get_docgen_token()
+    const token = await get_docgen_token()
     console.log(token)
 }
 
