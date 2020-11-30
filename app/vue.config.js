73f2cf8398fd5b1a464028a974f5960e56528f5f
Derek Roberts <derek.roberts@gmail.com>
2020-11-30 14:42:37 -0800

    Vue config - devtool source-map

--- app/vue.config.js
+++ app/vue.config.js
@@ -2,6 +2,7 @@ process.env.VUE_APP_VERSION = require('./package.json').version;
 
 module.exports = {
   configureWebpack: {
+    devtool: "source-map",
     devServer: {
         clientLogLevel: 'debug',
         watchOptions: {
