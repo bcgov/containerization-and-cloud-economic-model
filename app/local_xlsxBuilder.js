b113600196a59a46c25532f431a3fd88dbab4cf7
Derek Roberts <derek.roberts@gmail.com>
2020-12-07 18:44:04 -0800

    Moved authentication into commonServicesToolkit

--- app/local_xlsxBuilder.js
+++ app/local_xlsxBuilder.js
@@ -1,29 +1,19 @@
 // Requires
 const cstk = require('./src/utils/commonServicesToolkit');
-const axios = require('axios').default;
-const { Promise } = require('core-js');
 
-// Envars (clip url trailing slashes)
-const CLIENT_ID = process.env.CMNSRV_CLIENTID;
-const CLIENT_SECRET = process.env.CMNSRV_CLIENTSECRET;
-const TOKEN_URL = process.env.KEYCLOAK_OIDC_ENDPOINT.replace(/\/$/, '');
+// Envars
 const CONTEXTS = process.env.PATH_CONTEXTS;
 const RECIPIENT = process.env.EMAIL_RECIPIENT;
 
 // Accepts a data dict and a path to an xlsx template and makes a request to CDOGS.
 // Returns the response content object that can be added to a starlette.responses.Response.
 async function templateToEmail() {
-  // Get auth token and setup Axios defaults
-  const token = await cstk.getToken(TOKEN_URL, CLIENT_ID, CLIENT_SECRET);
-  axios.defaults.headers.Authorization = `Bearer ${token}`;
-
   // Send template and contexts, receive completed spreadsheet
   const contexts = require(CONTEXTS);
-  const spreadsheet = await cstk.getDocument(contexts, token);
+  const spreadsheet = await cstk.getDocument(contexts);
 
   // Send spreadsheet by email
-  const checkSend = await cstk.sendFile(spreadsheet.data, RECIPIENT, token);
-  console.log(checkSend);
+  await cstk.sendFile(spreadsheet.data, RECIPIENT);
 }
 
 templateToEmail();
--- app/src/utils/commonServicesToolkit.js
+++ app/src/utils/commonServicesToolkit.js
@@ -1,9 +1,12 @@
 // Requires
 const axios = require('axios').default;
 const base64 = require('base-64');
-const fs = require('fs'); //TODO: remove fs
+const fs = require('fs');
 
 // Envars (clip url trailing slashes)
+const CLIENT_ID = process.env.CMNSRV_CLIENTID;
+const CLIENT_SECRET = process.env.CMNSRV_CLIENTSECRET;
+const TOKEN_URL = process.env.KEYCLOAK_OIDC_ENDPOINT.replace(/\/$/, '');
 const CDOGS_URL = process.env.CS_CDOGS_ENDPOINT.replace(/\/$/, '');
 const CHES_URL = process.env.CS_CHES_ENDPOINT.replace(/\/$/, '');
 const TEMPLATE = process.env.PATH_TEMPLATE;
@@ -11,25 +14,25 @@ const SPREADSHEET = process.env.SPREADSHEET;
 const SENDER = process.env.EMAIL_SENDER;
 
 // Get token from DocGen SSO
-function getToken(url, clientId, clientSecret) {
+function getToken() {
   // URL query string and config with headers
-  const data = `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`;
+  const data = `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`;
   const config = {
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
   };
-
   // Return access token from response
   return new Promise((resolve, reject) => {
     axios
-      .post(url, data, config)
+      .post(TOKEN_URL, data, config)
       .then((res) => resolve(res.data.access_token))
       .catch((err) => reject(err));
   });
 }
 
 // Return a completed document from a template and contexts
-async function getDocument(contexts, token) {
+async function getDocument(contexts) {
   // Setup axios
+  const token = await getToken();
   axios.defaults.headers.Authorization = `Bearer ${token}`;
   axios.defaults.baseURL = CDOGS_URL;
 
@@ -81,8 +84,9 @@ async function getDocument(contexts, token) {
 }
 
 // Send a file by email
-async function sendFile(file, recipient, token) {
+async function sendFile(file, recipient) {
   // Setup axios
+  const token = await getToken();
   axios.defaults.headers.Authorization = `Bearer ${token}`;
   axios.defaults.baseURL = CHES_URL;
 
@@ -94,9 +98,11 @@ async function sendFile(file, recipient, token) {
         resolve(res.statusText);
       })
       .catch((err) => {
-        reject(err.statusText);
+        console.log('here!');
+        reject(err);
       });
   });
+
   console.log('CHES Health/Auth:', haCHES);
 
   // Payload
