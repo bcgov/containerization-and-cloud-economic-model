47af432c26880f72bc247210dc60d2c16dbcafc0
Derek Roberts <derek.roberts@gmail.com>
2020-12-09 14:41:15 -0800

    Add dependencies, envars and sourcing of commonServicesToolkit.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,12 @@
 const http = require('http');
+const cstk = require('./src/utils/commonServicesToolkit');
+
+// Envars
+const PORT = process.env.PORT || 3000;
+
+// Temporary envars - TODO: get these values from frontend
+const CONTEXTS = process.env.PATH_CONTEXTS;
+const RECIPIENT = process.env.EMAIL_RECIPIENT;
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
@@ -11,6 +19,5 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000);
-
-console.log('Listening on port 3000...');
+server.listen(PORT);
+console.log(`Listening on port ${PORT}...`);
