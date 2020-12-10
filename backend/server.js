8ef644e7523ce94fb93df72792513e55fa73fa18
Derek Roberts <derek.roberts@gmail.com>
2020-12-09 16:18:57 -0800

    Early API now building and emailing spreadsheets

--- backend/server.js
+++ backend/server.js
@@ -7,17 +7,29 @@ const PORT = process.env.PORT || 3000;
 // Temporary envars - TODO: get these values from frontend
 const CONTEXTS = process.env.PATH_CONTEXTS;
 const RECIPIENT = process.env.EMAIL_RECIPIENT;
+const contexts = require(CONTEXTS);
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.write('CEM backend API');
-    res.end();
-  }
-  if (req.url === '/_health') {
-    res.write('OK');
-    res.end();
-  }
-});
+async function runServer() {
+  const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+      res.write('CEM backend API');
+      const spreadsheet = templateToEmail(contexts, RECIPIENT);
+      console.log(spreadsheet);
+      res.end();
+    }
+    if (req.url === '/_health') {
+      res.write('OK');
+      res.end();
+    }
+  });
 
-server.listen(PORT);
-console.log(`Listening on port ${PORT}...`);
+  server.listen(PORT);
+  console.log(`Listening on port ${PORT}...`);
+}
+
+// Send template and contexts, email returned file (spreadsheet)
+async function templateToEmail(contexts, recipient) {
+  return await cstk.templateToEmail(contexts, recipient);
+}
+
+runServer();
