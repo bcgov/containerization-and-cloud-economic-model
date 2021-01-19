1c98eefffa227d0e9ee942c9b433ef10a6f9d15e
Derek Roberts <derek.roberts@gmail.com>
2021-01-19 11:05:58 -0800

    Hard code backend_url into frontend (temporarily)

--- frontend/src/components/Step3.vue
+++ frontend/src/components/Step3.vue
@@ -83,7 +83,9 @@
 </template>
 
 <script>
-const BACKEND_URL = process.env.BACKEND_URL || 'http://cem-backend:3000';
+const BACKEND_URL =
+  process.env.BACKEND_URL ||
+  'https://cem-backend-csnr-devops-lab-deploy.pathfinder.gov.bc.ca:3000';
 import { mapActions, mapGetters, mapMutations } from 'vuex';
 
 import Step1 from '@/components/Step1.vue';
