aff6533164c9a479097a6658983c7b663088630b
Derek Roberts <derek.roberts@gmail.com>
2021-01-21 14:06:31 -0800

    Add deploy and tools namespaces to quickDeploy.sh

--- openshift/quickDeploy.sh
+++ openshift/quickDeploy.sh
@@ -9,8 +9,12 @@ GIT_BRANCH="${GIT_BRANCH:-$(git symbolic-ref --short -q HEAD)}"
 GIT_URL=https://github.com/${GIT_URL#*github.com[:/]} # replace all prefixes
 GIT_URL=${GIT_URL%.git}                               # trim trailing .git
 
+# OpenShift details
+NAMESPACE_DEPLOY=csnr-devops-lab-deploy
+NAMESPACE_TOOLS=csnr-devops-lab-tools
+
 # Create secret, if necessary
-oc get secret cem-backend -o name ||(
+oc get secret cem-backend -o name -n ${NAMESPACE_DEPLOY} ||(
   echo -e "\nSecret not present.  CLIENT_ID and CLIENT_SECRET not found."
   echo -e "\nSign up for credentials at https://getok.nrs.gov.bc.ca.\n"
 
@@ -18,15 +22,15 @@ oc get secret cem-backend -o name ||(
   CLIENT_ID="${CLIENT_ID:-CEM_SERVICE_CLIENT}"
   read -p "CLIENT_SECRET:" CLIENT_SECRET
   echo
-  oc process -f backend.secret.yml -p CLIENT_ID=${CLIENT_ID} -p CLIENT_SECRET=${CLIENT_SECRET} | oc apply -f -
+  oc process -f deploy-backend.secret.yml -p CLIENT_ID=${CLIENT_ID} -p CLIENT_SECRET=${CLIENT_SECRET} | oc apply -f -
 )
 
 # Create builds and deployments
-oc process -f backend.build.yml -p GIT_URL=${GIT_URL} -p GIT_BRANCH=${GIT_BRANCH} | oc apply -f -
-oc process -f frontend.build.yml -p GIT_URL=${GIT_URL} -p GIT_BRANCH=${GIT_BRANCH} | oc apply -f -
-oc process -f backend.deploy.yml | oc apply -f -
-oc process -f frontend.deploy.yml | oc apply -f -
+oc process -f tools-backend.bc.yml -p GIT_URL=${GIT_URL} -p GIT_BRANCH=${GIT_BRANCH} | oc apply -f -
+oc process -f tools-frontend.bc.yml -p GIT_URL=${GIT_URL} -p GIT_BRANCH=${GIT_BRANCH} | oc apply -f -
+oc process -f deploy-backend.dc.yml | oc apply -f -
+oc process -f deploy-frontend.dc.yml | oc apply -f -
 
 # Start builds, following the longest (deployments triggered by build completions)
-oc start-build cem-backend
-oc start-build cem-frontend --follow
+oc start-build cem-backend -n ${NAMESPACE_TOOLS}
+oc start-build cem-frontend -n ${NAMESPACE_TOOLS} --follow
